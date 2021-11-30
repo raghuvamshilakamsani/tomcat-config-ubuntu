fb0b186e82170febb87241eb4b4212d9761a0fa5
kmashwin <52381591+kmashwin@users.noreply.github.com>
2021-11-30 19:33:21 +0530

    removing files in destination before copying
    removing context.xml in manager and host manager and removing tomcat-users.xml in conf before copying to that folder

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -14,6 +14,9 @@ sudo tar -xvzf apache-tomcat-9.*
 sudo rm -rf apache*.gz
 sudo chmod -R 755 /home/$USER/tomcat${version}/apache-tomcat-9.*
 sudo sleep 3
+sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
+sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
+sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/tomcat-users.xml
 sudo cp ./tomcat-config/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
 sudo cp ./tomcat-config/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
 sudo cp ./tomcat-config/tomcat-users.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/tomcat-users.xml
