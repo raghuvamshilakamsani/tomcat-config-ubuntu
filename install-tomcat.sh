f856ea62ea9e280a43d372b0eca895b803f8b873
kmashwin <52381591+kmashwin@users.noreply.github.com>
2021-11-30 17:53:43 +0530

    adding sudo to mkdir and rm
    adding sudo to mkdir and rm

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -5,9 +5,9 @@ version=`echo $ver | cut -d " " -f2`
 echo $version
 sudo apt install -y default-jdk wget git
 cd /home/$USER/
-mkdir tomcat${version}
+sudo mkdir tomcat${version}
 cd /home/$USER/tomcat${version}
-rm -rf apache*
+sudo rm -rf apache*
 sudo wget https://downloads.apache.org/tomcat/tomcat-9/v${version}/bin/apache-tomcat-${version}.tar.gz
 sudo tar -xvzf apache-tomcat-9.*
 sudo rm -rf apache*.gz
