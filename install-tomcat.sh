3ce227bfee9094b313ff682e17689ec8da80d637
kmashwin <52381591+kmashwin@users.noreply.github.com>
2021-11-30 20:40:46 +0530

    adding verbose to cp
    adding verbose to copy command

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -5,8 +5,8 @@ version=`echo $ver | cut -d " " -f2`
 echo $version
 sudo apt update
 sudo apt install -y default-jdk wget git
-sudo cp context.xml /home/$USER/
-sudo cp tomcat-users.xml /home/$USER/
+sudo cp -v context.xml /home/$USER/
+sudo cp -v tomcat-users.xml /home/$USER/
 cd /home/$USER/
 sudo mkdir tomcat${version}
 cd /home/$USER/tomcat${version}
@@ -18,9 +18,9 @@ sudo chmod -R 755 /home/$USER/tomcat${version}/apache-tomcat-9.*
 sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
 sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
 sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/tomcat-users.xml
-sudo cp /home/$USER/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/
-sudo cp /home/$USER/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/
-sudo cp /home/$USER/tomcat-users.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/
+sudo cp -v /home/$USER/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/
+sudo cp -v /home/$USER/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/
+sudo cp -v /home/$USER/tomcat-users.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/
 sudo rm -rf /home/$USER/context.xml
 sudo rm -rf /home/$USER/tomcat-users.xml
 cd /home/$USER/tomcat${version}/apache-tomcat-9.*
