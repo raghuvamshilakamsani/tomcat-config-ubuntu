ff42c632af7f5ac97b38f117aca8a67360594f0d
NIKHIL VARMA M <63706239+nikhilvarma940@users.noreply.github.com>
2022-01-26 10:35:49 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -6,22 +6,23 @@ echo $version
 sudo apt update
 sudo apt install -y default-jdk wget git
 cd /home/$USER/
-sudo mkdir tomcat${version}
-cd /home/$USER/tomcat${version}
+mkdir opt/
+sudo mkdir /opt/tomcat${version}
+cd /home/$USER/opt/tomcat${version}
 sudo wget https://downloads.apache.org/tomcat/tomcat-9/v${version}/bin/apache-tomcat-${version}.tar.gz
 sudo tar -xvzf apache-tomcat-9.*
 sudo rm -rf apache*.gz
-sudo chmod -R 755 /home/$USER/tomcat${version}/apache-tomcat-9.*
-sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
-sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
-sudo rm -rfv /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/tomcat-users.xml
-sudo cp -v /home/$USER/tomcat-config-ubuntu/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/
-sudo cp -v /home/$USER/tomcat-config-ubuntu/context.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/
-sudo cp -v /home/$USER/tomcat-config-ubuntu/tomcat-users.xml /home/$USER/tomcat${version}/apache-tomcat-9.*/conf/
+sudo chmod -R 755 /home/$USER/opt/tomcat${version}/apache-tomcat-9.*
+sudo rm -rfv /home/$USER/opt/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
+sudo rm -rfv /home/$USER/opt/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
+sudo rm -rfv /home/$USER/opt/tomcat${version}/apache-tomcat-9.*/conf/tomcat-users.xml
+sudo cp -v /home/$USER/tomcat-config-ubuntu/context.xml /home/$USER/opt/tomcat${version}/apache-tomcat-9.*/webapps/manager/META-INF/
+sudo cp -v /home/$USER/tomcat-config-ubuntu/context.xml /home/$USER/opt/tomcat${version}/apache-tomcat-9.*/webapps/host-manager/META-INF/
+sudo cp -v /home/$USER/tomcat-config-ubuntu/tomcat-users.xml /home/$USER/opt/tomcat${version}/apache-tomcat-9.*/conf/
 sudo rm -rf /home/$USER/tomcat-config-ubuntu
-cd /home/$USER/tomcat${version}/apache-tomcat-9.*
+cd /home/$USER/opt/tomcat${version}/apache-tomcat-9.*
 sudo sh bin/startup.sh
 echo "username: admin"
 echo "password: admin"
-sudo chown $USER:ubuntu /home/$USER/tomcat${version}
-sudo chown $USER:ubuntu /home/$USER/tomcat${version}/apache-tomcat-9.*
+sudo chown $USER:ubuntu /home/$USER/opt/tomcat${version}
+sudo chown $USER:ubuntu /home/$USER/opt/tomcat${version}/apache-tomcat-9.*
